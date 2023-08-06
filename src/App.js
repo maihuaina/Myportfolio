9fa44970696c66a81122fc28e5de3f6b434f67b9
Aina <ainaaali1999@gmail.com>
2023-08-06 19:59:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useCallback } from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Skills from "./Components/Skills";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container m-auto">
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Contact />
+      <Footer />
     </div>
   );
 }
